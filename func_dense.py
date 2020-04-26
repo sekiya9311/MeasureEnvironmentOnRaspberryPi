9efd86e3f662d62b0f27ffdc7a9547d813d77cce
sekiya9311 <sekiya9311@gmail.com>
2020-04-26 23:37:00 +0900

    add func of put log

--- func_dense.py
+++ func_dense.py
@@ -2,9 +2,16 @@ import mh_z19
 import json
 import urllib.request
 import time
+import datetime
 
+def put_log(data):
+    cur_date = datetime.datetime.today()
+    dump_date = json.dumps(data)
+    with open('logs/{0:%Y%m%d}.txt'.format(cur_date), 'a') as f:
+        f.write('[{0:%H:%M:%S}]:{1}\n'.format(cur_date, dump_date))
 
 def post_value(data):
+    put_log(data)
     URL = 'https://us-central1-measureenvironments.cloudfunctions.net/addCO2'
     HEADERS = {
         'Content-type': 'application/json'
